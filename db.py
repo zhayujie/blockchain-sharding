15bac625360d4d20680478c85757d24779217c80
zhayujie <yjzha1996@163.com>
2018-12-21 14:59:40 +0800

    基于psycopg2实现python连接PG数据库模块

---
+++ db.py
@@ -0,0 +1,92 @@
+# -*- coding:utf-8 -*-
+# Sharding v0.1
+
+import psycopg2
+
+#groups = {0:3, 1:3, 2:5, 3: 5, 4:5, 6: 99, 7: 89, 8:99, 9:89}
+
+TABLE_NAME = 'groups_temp'
+
+
+def get_connect():
+    conn = psycopg2.connect(database="sharding", user="postgres", password="soraru11",
+                            host="127.0.0.1", port="5432")
+    return conn
+
+
+# 读取用于测试的交易
+def get_new_txs(limit, offset):
+    res = []
+    try:
+        conn = get_connect()
+        cursor = conn.cursor()
+        cursor.execute('SELECT DISTINCT tx_id FROM txtest LIMIT {}'.format(limit))
+        txs = cursor.fetchall()             # [(txid1), (txid2) ...]
+        for tx_id in txs:
+            cursor.execute('SELECT * FROM txtest WHERE tx_id = {}'.format(tx_id[0]))
+            results = cursor.fetchall()     # [(txid1, in1, out1), (txid1, ...), (txid2 ...)]
+            for r in results:
+                res.append(r)
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+    return res
+
+    #return ((123, 2, 3), (123, 3, 5), (123, 2, 5),(125, 7, 9), (126, 8, 3))
+
+
+# 根据 addressId 查询 groupId，不存在返回-1
+def get_group_id(addr_id):
+    """
+    group_id = groups.get(addr_id)
+    if not group_id:
+        return -1
+    return group_id
+    """
+    sql = 'SELECT groupid FROM {} WHERE addressid = {}'.format(TABLE_NAME, addr_id)
+    try:
+        conn = get_connect()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        result = cursor.fetchone()
+    except Exception as e:
+        print(e)
+    else:
+        if result:
+            res = result[0]
+        else:
+            res = -1
+    finally:
+        cursor.close()
+        conn.close()
+
+    return res
+
+
+# 向分组索引表中插入新的地址
+def insert_new_tx(addr_id, group_id):
+    sql = 'INSERT INTO {} (addressid, groupid) VALUES ({}, {})'.format(TABLE_NAME, addr_id, group_id)
+    try:
+        conn = get_connect()
+        cursor = conn.cursor()
+        cursor.execute(sql)
+        conn.commit()
+    except Exception as e:
+        print(e)
+        if conn:
+            conn.rollback()
+    finally:
+        cursor.close()
+        conn.close()
+    return 0
+
+
+if __name__ == '__main__':
+    print(get_group_id(1396600))
+
+    insert_new_tx(99999999, 77)
+
+    print(get_new_txs(100))
+
